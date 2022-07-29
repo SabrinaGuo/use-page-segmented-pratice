fd0c80f73213c5813da75ce72b5c7967a2c3890c
sabrinaguo <hsif0111@gmail.com>
2022-07-29 10:33:29 +0800

    Initial Commit

---
+++ use-page-segmented-pratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  use-page-segmented-pratice
+//
+//  Created by 郭怡玲 on 2022/7/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
